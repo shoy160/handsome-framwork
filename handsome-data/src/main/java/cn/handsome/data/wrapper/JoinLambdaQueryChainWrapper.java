60c51e648a50082426c679b5d6535d03afb7ec57
shoy160 <shoy160@qq.com>
2022-01-03 21:02:10 +0800

    add join lambda

---
+++ handsome-data/src/main/java/cn/handsome/data/wrapper/JoinLambdaQueryChainWrapper.java
@@ -0,0 +1,38 @@
+package cn.handsome.data.wrapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+
+/**
+ * 支持连表查询的LambdaWrapper
+ *
+ * @author shay
+ * @date 2022/1/1
+ **/
+public class JoinLambdaQueryChainWrapper<T> extends LambdaQueryChainWrapper<T> {
+    public JoinLambdaQueryChainWrapper(BaseMapper<T> baseMapper) {
+        super(baseMapper);
+    }
+
+    public <S> JoinLambdaQueryChainWrapper<T> selectAs(SFunction<S, ?> column, String alias) {
+        // TODO: 2022/1/1
+        return this;
+    }
+
+    public final JoinLambdaQueryChainWrapper<T> selectAll(Class<?> clazz) {
+        return this;
+    }
+
+    public <R> JoinLambdaQueryChainWrapper<T> join(String keyWord, boolean condition, Class<R> clazz, OnFunction function) {
+        if (condition) {
+//            MPJLambdaWrapper<?> apply = function.apply(instance(keyWord, clazz));
+//            onWrappers.add(apply);
+//            subTable.put(clazz, tableIndex);
+//            tableIndex++;
+        }
+        return this;
+    }
+
+}
